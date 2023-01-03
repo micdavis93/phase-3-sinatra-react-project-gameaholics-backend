cc699e5dfaa8fed6468d240e350a06cace6e1b63
Michael Davis <micdavis93@gmail.com>
2023-01-03 09:52:47 -0700

    added general backend pathway with all data in one JSON object

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
+  get '/' do
+    games = Game.all
+    gamers = Gamer.all
+    reviews = Review.all
+    {games: games, gamers: gamers, reviews: reviews}.to_json
+  end
+  
   get '/games' do
     games = Game.all
     games.to_json
